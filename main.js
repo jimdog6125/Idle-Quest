56c7b04401639d47c12d7473c47bea7a5d016b49
jimdog6125 <24468320+jimdog6125@users.noreply.github.com>
2018-03-14 16:08:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var kills = 0;
 var coins = 0;
-
+var stuffPs = 0;
 function kill(number){
     kills = kills + number;
     coins = coins + number;
@@ -15,7 +15,8 @@ function buySword(){
     var swordCost = Math.floor(10 * Math.pow(1.1,swords));     //works out the cost of this cursor
     if(coins >= swordCost){                                   //checks that the player can afford the cursor
         swords = swords + 1;                                   //increases number of cursors
-    	coins = coins - swordCost;                          //removes the cookies spent
+    	coins = coins - swordCost;
+	stuffPs = stufPs + 0.5;
         document.getElementById('swords').innerHTML = swords;  //updates the number of cursors for the user
         document.getElementById('coins').innerHTML = coins;  //updates the number of cookies for the user
     };
@@ -29,7 +30,8 @@ function buyMagic(){
     var magicCost = Math.floor(100 * Math.pow(1.1,magic));     //works out the cost of this cursor
     if(coins >= magicCost){                                   //checks that the player can afford the cursor
         magic = magic + 1;                                   //increases number of cursors
-    	coins = coins - magicCost;                          //removes the cookies spent
+    	coins = coins - magicCost;
+	    stuffPs = stufPs + 1;
         document.getElementById('magic').innerHTML = magic;  //updates the number of cursors for the user
         document.getElementById('coins').innerHTML = coins;  //updates the number of cookies for the user
     };
@@ -41,8 +43,9 @@ var cannons = 0;
 function buyCannon(){
     var cannonCost = Math.floor(500 * Math.pow(1.1,cannons));     //works out the cost of this cursor
     if(coins >= cannonCost){                                   //checks that the player can afford the cursor
-        cannons = cannons + 1;                                   //increases number of cursors
-    	coins = coins - cannonCost;                          //removes the cookies spent
+        cannons = cannons + 2;                                   //increases number of cursors
+    	coins = coins - cannonCost;
+	    stuffPs = stufPs + 2;//removes the cookies spent
         document.getElementById('cannons').innerHTML = cannons;  //updates the number of cursors for the user
         document.getElementById('coins').innerHTML = coins;  //updates the number of cookies for the user
     };
@@ -55,7 +58,8 @@ function buyTower(){
     var towerCost = Math.floor(5000 * Math.pow(1.1,towers));     //works out the cost of this cursor
     if(coins >= towerCost){                                   //checks that the player can afford the cursor
         towers = towers + 1;                                   //increases number of cursors
-    	coins = coins - towerCost;                          //removes the cookies spent
+    	coins = coins - towerCost;    
+	   stuffPs = stufPs + 4;
         document.getElementById('towers').innerHTML = towers;  //updates the number of cursors for the user
         document.getElementById('coins').innerHTML = coins;  //updates the number of cookies for the user
     };
@@ -69,7 +73,8 @@ function buyAMagic(){
     var aMagicCost = Math.floor(15000 * Math.pow(1.1,ancientMagic));     //works out the cost of this cursor
     if(coins >= aMagicCost){                                   //checks that the player can afford the cursor
         ancientMagic = ancientMagic + 1;                                   //increases number of cursors
-    	coins = coins - aMagicCost;                          //removes the cookies spent
+    	coins = coins - aMagicCost;    
+	    stuffPs = stufPs + 10;
         document.getElementById('ancientMagic').innerHTML = ancientMagic;  //updates the number of cursors for the user
         document.getElementById('coins').innerHTML = coins;  //updates the number of cookies for the user
     };
@@ -107,7 +112,7 @@ var lastUpdate = Date.now()
 setInterval(function() {
     let currentUpdate = Date.now()
     let delta = (currentUpdate - lastUpdate) / 1000 //divided by 1000 because Date.now() is in milliseconds
-    currency += resourcesGainedPerSecond * delta
+    coins += stufPs * delta
 
    lastUpdate = currentUpdate
 }, 100)
